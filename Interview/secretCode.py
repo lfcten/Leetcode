1985801baa350c909ff1eafb5b98ba71d4af12a3
lfcten <664629957@qq.com>
2018-04-02 22:16:01 +0800

    Create secretCode.py

---
+++ Interview/secretCode.py
@@ -0,0 +1,27 @@
+import bisect
+
+"""
+在线用的是python2.7
+竟然忘了python2.7 float运算需要 * 1.0
+提交了一个错答案，过了8/9 哈哈
+"""
+def main(num, secretCode):
+    secretCode.sort()
+    if num == 0:
+        return secretCode[0]
+    total = sum(secretCode)
+    for i in range(num - 1):
+        target = total / (num - i)
+        ind = bisect.bisect(secretCode, target)
+        if abs(secretCode[ind] - target) >= abs(secretCode[ind - 1] - target):
+            val = secretCode.pop(ind - 1)
+        else:
+            val = secretCode.pop(ind)
+        total -= val
+    return secretCode[0]
+
+
+if __name__ == "__main__":
+    print(main(4, [1, 2, 7, 100]))
+    print(main(4, [3, 4, 5, 2]))
+    print(main(3, [1, 4, 10000]))
